afb8c25be7797b9be7c2a2f9be6f347c4cd66d5b
Niu Haoyu <niuhaoyu17@gmail.com>
2019-07-16 17:08:00 +0800

    添加了开始页面和点击跳转

---
+++ assets/scripts/start.js
@@ -0,0 +1,63 @@
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+        bg: {
+            default: null,
+            type: cc.Node
+        },  
+
+        playButton: {
+            default: null,
+            type: cc.Node
+        },
+
+        scoreButton: {
+            default: null,
+            type: cc.Node
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad: function () {
+        this.bg.setContentSize(this.node.width, this.node.height)
+        this.playButton.on(cc.Node.EventType.TOUCH_END, function (event) {
+            cc.director.loadScene("game")
+        })
+        this.scoreButton.on(cc.Node.EventType.TOUCH_END, function (event) {
+            cc.director.loadScene("highScores")
+        })
+    },
+
+    start () {
+
+    },
+
+    update (dt) {},
+});
